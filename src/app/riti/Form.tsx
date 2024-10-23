59df8aac186cdd6f5ecc2e5870ce6ff0919c1e4e
bitch <renki22han@gmail.com>
2024-10-23 14:58:09 +0530

    Initial commit

---
+++ src/app/riti/Form.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <section>
+        <div className="flex flex-col gap-4  items-center">
+            <div className=' w-80'>heading</div>
+            <input className='w-80 bg-transparent border border-orange-400 h-6 p-5' placeholder='p1'/>
+            
+            <div className=' w-80'>heading</div>
+            <input className='w-80 bg-transparent border border-orange-400 h-6 p-5' placeholder='p1'/>
+            
+            <div className=' w-80'>heading</div>
+            <input className='w-80 bg-transparent border border-orange-400 h-6 p-5' placeholder='p1'/>
+            
+            <div className=' w-80'>heading</div>
+            <input className='w-80 bg-transparent border border-orange-400 h-6 p-5' placeholder='p1'/>
+            
+            <div className=' w-80'>heading</div>
+            <input className='w-80 bg-transparent border border-orange-400 h-6 p-5' placeholder='p1'/>
+            
+            <div className=' w-80'>heading</div>
+            <input className='w-80 bg-transparent border border-orange-400 h-6 p-5' placeholder='p1'/>
+            
+            <div className=' w-80'>heading</div>
+            <input className='w-80 bg-transparent border border-orange-400 h-6 p-5' placeholder='p1'/>
+            
+            <div className=' w-80'>heading</div>
+            <input className='w-80 bg-transparent border border-orange-400 h-6 p-5' placeholder='p1'/>
+            
+
+        </div>
+    </section>
+  )
+}
+
+export default Form
---
+++ src/app/riti/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from '@/sections/Header'
+import Form from './Form'
+function page() {
+  return (
+    <>
+    <div>
+      <Header/>
+      <Form/>
+    </div>
+    </>
+  )
+}
+
+export default page
